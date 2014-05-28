5e18610a0653aadb910a39870e78ad6f6404a606
Adrian <Adrian@Adrian-PC>
2014-05-28 20:56:05 +0200

    Subida del proyecto

---
+++ MissingWords/android/src/com/mygdx/MissingWords/android/AndroidLauncher.java
@@ -0,0 +1,16 @@
+package com.mygdx.MissingWords.android;
+
+import android.os.Bundle;
+
+import com.badlogic.gdx.backends.android.AndroidApplication;
+import com.badlogic.gdx.backends.android.AndroidApplicationConfiguration;
+import com.mygdx.MissingWords.MissingWords;
+
+public class AndroidLauncher extends AndroidApplication {
+	@Override
+	protected void onCreate (Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		AndroidApplicationConfiguration config = new AndroidApplicationConfiguration();
+		initialize(new MissingWords(), config);
+	}
+}
